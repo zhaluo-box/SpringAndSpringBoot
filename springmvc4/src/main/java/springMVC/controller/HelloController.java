d04089a64d67a7c011753414ac9311c6a3c6fb58
zhaluobox@163.com <zhaluobox@163.com>
2018-11-05 16:00:07 +0800

    重写MVC

--- springmvc4/src/main/java/springMVC/controller/HelloController.java
+++ springmvc4/src/main/java/springMVC/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping("/index")
